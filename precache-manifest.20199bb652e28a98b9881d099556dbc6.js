3f105c7f62dbce031cfc9d304c293dfb58917458
a_soni <a.soni@eicare.org>
2021-02-16 23:43:25 +0530

    Updates

---
+++ precache-manifest.20199bb652e28a98b9881d099556dbc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f20e904f9e7ff881b7c2774b4bf35f",
+    "url": "/PIC-Search-App/index.html"
+  },
+  {
+    "revision": "46731532afdc9d7f9b38",
+    "url": "/PIC-Search-App/static/css/main.18e0b75d.chunk.css"
+  },
+  {
+    "revision": "60c9b27da1bfda77948c",
+    "url": "/PIC-Search-App/static/js/2.172aecc3.chunk.js"
+  },
+  {
+    "revision": "46731532afdc9d7f9b38",
+    "url": "/PIC-Search-App/static/js/main.229b15f2.chunk.js"
+  },
+  {
+    "revision": "7fd81fd2b3c7cec5c43e",
+    "url": "/PIC-Search-App/static/js/runtime-main.9209c9f1.js"
+  }
+]);
